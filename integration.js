7ae6bcdfe1187eaaf867610f774a1d6d878fb89b
Nathan Jenan <nathan.jenan@polarity.io>
2018-03-31 21:32:46 -0700

    adding basic lookup no display

--- integration.js
+++ integration.js
@@ -9,7 +9,110 @@ function getRequestOptions() {
 }
 
 function doLookup(entities, options, callback) {
-    callback(null, null);
+    let results = [];
+
+    async.each(entities, (entity, done) => {
+        let requestOptions = getRequestOptions();
+        requestOptions.url = 'https://caws3.nsslabs.com/graphql';
+        requestOptions.method = 'POST';
+        requestOptions.headers = {
+            Authorization: options.apiKey
+        };
+        requestOptions.body = {
+            query: `query { 
+                threatInfo(ipAddresses: ["${entity.value}"]) {
+                    exploits {
+                        nssid
+                    }
+                }
+            }`
+        };
+
+        request(requestOptions, (err, resp, body) => {
+            if (err || resp.statusCode != 200) {
+                Logger.error({ err: err, body: body });
+                done(err || new Error('resp code was not 200 ' + body))
+            }
+
+            let threatInfo = body.data.threatInfo;
+            if (threatInfo.exploits.length === 0) {
+                results.push({
+                    entity: entity,
+                    data: null
+                });
+                done();
+            } else {
+                async.each(body.data.threatInfo.exploits, (exploit, done) => {
+                    requestOptions.body = {
+                        query: `query {
+                            threatIntel(nssid: ${exploit.nssid}) {
+                                details {
+                                    exploitType
+                                    discoveryDate
+                                    threatType
+                                    url
+                                    md5FileHash
+                                    application {
+                                        daysAtRisk
+                                        application {
+                                            name
+                                            family
+                                            vendor {
+                                                name
+                                            }
+                                        }
+                                    }
+                                    platform {
+                                        daysAtRisk
+                                        platform {
+                                            name
+                                            family
+                                            vendor {
+                                                name
+                                            }
+                                        }
+                                    }
+                                    browser {
+                                        daysAtRisk
+                                        browser {
+                                            name
+                                            family
+                                            vendor {
+                                                name
+                                            }
+                                        }
+                                    }
+                                    source {
+                                        exploitType
+                                        discoveryDate
+                                        threatType
+                                        device {
+                                            name
+                                            type
+                                            vendor {
+                                                name
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }`
+                    };
+                    request(requestOptions, (err, resp, body) => {
+                        if (err || resp.statusCode != 200) {
+                            Logger.error({ err: err, body: body });
+                            done(err || new Error('resp code was not 200 ' + body))
+                        }
+
+                        Logger.trace({ body: body });
+                        done();
+                    });
+                });
+            }
+        });
+    }, err => {
+        callback(err, results);
+    });
 }
 
 function startup(logger) {
@@ -41,7 +144,17 @@ function startup(logger) {
 }
 
 function validateOptions(options, callback) {
-    callback(null, null);
+    let errors = [];
+
+    if (typeof options.apiKey.value !== 'string' ||
+        (typeof options.apiKey.value === 'string' && options.apiKey.value.length === 0)) {
+        errors.push({
+            key: "apiKey",
+            message: errMessage
+        });
+    }
+
+    callback(null, errors);
 }
 
 module.exports = {
